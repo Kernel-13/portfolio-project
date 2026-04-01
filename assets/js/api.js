77e5b80082b66f55609c12d7da3ff31746283396
K. Thirteen <funescastilloederson@gmail.com>
2026-04-01 21:33:00 +0200

    feat(app): add 7-day weather forecast under current weather info

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,34 @@
+export const WMO_WEATHER_CODES = {
+  0: "Clear sky",
+  1: "Mainly clear",
+  2: "Partly cloudy",
+  3: "Overcast",
+  45: "Fog",
+  48: "Depositing rime fog",
+  51: "Drizzle: Light intensity",
+  53: "Drizzle: Moderate intensity",
+  55: "Drizzle: Dense intensity",
+  56: "Freezing Drizzle: Light intensity",
+  57: "Freezing Drizzle: Dense intensity",
+  61: "Rain: Slight intensity",
+  63: "Rain: Moderate intensity",
+  65: "Rain: Heavy intensity",
+  66: "Freezing Rain: Light intensity",
+  67: "Freezing Rain: Heavy intensity",
+  71: "Snow fall: Slight intensity",
+  73: "Snow fall: Moderate intensity",
+  75: "Snow fall: Heavy intensity",
+  77: "Snow grains",
+  80: "Rain showers: Slight",
+  81: "Rain showers: Moderate",
+  82: "Rain showers: Violent",
+  85: "Snow showers: Slight",
+  86: "Snow showers: Heavy",
+  95: "Thunderstorm: Slight or moderate",
+  96: "Thunderstorm with slight hail",
+  99: "Thunderstorm with heavy hail"
+};
+
 export async function getLocation(city) {
     try {
         let response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`)
@@ -39,13 +70,13 @@ export async function getLocationTwo(event) {
 
 export async function getWeatherData(lat, lon) {
     try {
-        let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lat, lon}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`)
+        let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`)
         if (!response.ok) {
             console.error('Error: Open-Meteo Server is not returning the expected response')
             return
         }
 
-        let data = response.json()
+        let data = await response.json()
         return data
     } catch (error) {
         console.error(error)        
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 import { debouncer } from "./utils.js";
-import { getLocation, getWeatherData } from "./api.js";
+import { getLocation, getWeatherData, WMO_WEATHER_CODES } from "./api.js";
 
 document.getElementById('search-btn').addEventListener('click', async () => {
     
     const spinner = document.getElementsByClassName('loading-spinner')[0]
     const cityInput = document.getElementById("city-input")
+    const forecastGrid = document.getElementById('forecast-container')
     
     spinner.style.display = 'block'
 
@@ -23,10 +24,45 @@ document.getElementById('search-btn').addEventListener('click', async () => {
             spinner.style.display = 'none'
             const temperatureUnits = weatherData.current_units.temperature_2m
             cityName.textContent = location.name
-            cityDescription.textContent =  weatherData.timezone
+            cityDescription.textContent =  WMO_WEATHER_CODES[weatherData.current.weather_code] ?? 'Unknown description'
             cityTemp.textContent = String(weatherData.current.temperature_2m) + temperatureUnits
             cityHumidity.textContent = String(weatherData.current.relative_humidity_2m)
-            cityWind.textContent = String(weatherData.current.relative_humidity_2m)
+            cityWind.textContent = String(weatherData.current.wind_speed_10m)
+
+            const forecastInfo = weatherData.daily
+            console.log(forecastInfo)
+            const forcastLength = forecastInfo.time.length
+            const fragmentDoc = new DocumentFragment()
+            forecastGrid.innerHTML = ''
+
+            for (let i = 0; i < forcastLength; i++) {
+                let forecastDate = new Date(forecastInfo.time[i])
+
+                let formattedDate = forecastDate.toLocaleDateString("en-US", {
+                    weekday: "short",
+                    day: "numeric",
+                    month: "short"
+                })
+
+                let template = document.createElement('template')
+
+                template.innerHTML = `
+                <div class="forecast-card">
+                    <p class="forecast-date">${formattedDate}</p>
+                    <i alt="Weather Icon" class="wi wi-wmo4680-${forecastInfo.weather_code[i]}"></i>
+                    <div class="forecast-temps">
+                        <span class="high">${forecastInfo.temperature_2m_max[i]}°C</span>
+                        <span class="low">${forecastInfo.temperature_2m_min[i]}°C</span>
+                    </div>
+                    <p class="forecast-desc">${WMO_WEATHER_CODES[forecastInfo.weather_code[i]]}</p>
+                </div>
+                `.trim()
+                
+                fragmentDoc.append(template.content)
+            }
+
+           forecastGrid.append(fragmentDoc)
+
         }
     }
     
